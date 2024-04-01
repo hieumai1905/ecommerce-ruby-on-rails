54b14a2a71c4fc1b290bc70d739e42aef37dca22
hieu mai <mai.van.hieu@sun-asterisk.com>
2024-04-01 09:10:11 +0700

    Init project and add template

---
+++ config/application.rb
@@ -0,0 +1,17 @@
+require_relative "boot"
+
+require "rails/all"
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module Test
+  class Application < Rails::Application
+    config.load_defaults 7.1
+    Bundler.require(*Rails.groups)
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
+    config.i18n.available_locales = [:en, :vi]
+    config.i18n.default_locale = :vi
+  end
+end

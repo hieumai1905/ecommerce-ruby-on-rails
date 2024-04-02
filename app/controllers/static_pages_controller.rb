d2465c4285be0b13c8757e4482b8ba411b30c59f
hieu mai <mai.van.hieu@sun-asterisk.com>
2024-04-02 23:45:48 +0700

    show banner

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,3 +1,11 @@
 class StaticPagesController < ApplicationController
+  before_action :load_banner, only: :home
   def home; end
+
+  private
+  def load_banner
+    @default_banner = Banner.new photo_path: "hero/default.jpg",
+                                 description: t("pages.home.welcome")
+    @banner = Banner.current_banners
+  end
 end
--- app/models/banner.rb
+++ app/models/banner.rb
@@ -1,2 +1,5 @@
 class Banner < ApplicationRecord
+  scope :current_banners, (lambda do
+    where("start_at <= ? AND finish_at > ?", Time.zone.today, Time.zone.today)
+  end)
 end
